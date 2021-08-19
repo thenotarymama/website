7cab11aa39bf73fce24a95751de9762d639804cd
sumit <mandal.sumit00@gmail.om>
2021-08-19 19:47:28 +0530

    first commit

---
+++ simpleblog/ablog/theblog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path,include
+# from . import views
+from .views import HomeView,ArticleDetailView,Codepen,AddPostView,UpdatePostView,DeletePostView
+
+urlpatterns = [
+    # path('',views.home, name="home"),
+    path('',HomeView.as_view(),name='home'),
+    path('codepen',Codepen.as_view(),name='codepen'),
+    path('article/<int:pk>',ArticleDetailView.as_view(),name='article-detail'),
+    path('add_post/', AddPostView.as_view(),name='add_post'),
+    path('article/edit/<int:pk>',UpdatePostView.as_view(),name='update_post'),
+    path('article/<int:pk>/remove',DeletePostView.as_view(),name='delete_post'),
+
+
+]
---
+++ simpleblog/ablog/theblog/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from .models import Post
+from .forms import PostForm,EditForm
+from django.urls import reverse_lazy
+
+# Create your views here.
+# def home(request):
+#     return render(request,'home.html',{})
+
+class HomeView(ListView):
+	model = Post
+	template_name = 'home.html'
+	ordering = ['-id']
+	# cats = Category.objects.all()
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
+
+class Codepen(ListView):
+	model = Post
+	template_name='main2.html'
+
+class AddPostView(CreateView):
+	model = Post
+	form_class = PostForm
+	template_name = 'add_post.html'
+	# fields = '__all__'
+	# fields = ('title','body')
+class UpdatePostView(UpdateView):
+	model = Post
+	form_class = EditForm
+	template_name = 'update_post.html'
+	# fields = ['title','title_tag','body']
+class DeletePostView(DeleteView):
+	model = Post
+	template_name = 'delete_post.html'
+	success_url = reverse_lazy('home')
